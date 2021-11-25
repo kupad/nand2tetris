e7f64c27378a1f55ee6f89b3f2f678fbe237c32a
kupad <dreizen@kupad.net>
2021-11-24 19:19:32 -0500

    first Fill attempt
    While a key is pressed, the screen begins to fill with black. While no
    key is pressed, it begins to fill with white.
    
    I'm not sure if this is what they had in mind? As in: should I fill out
    the whole screen as soon as a keypress is detected?
    
    The book suggests that my pattern is okay. Will probably watch the
    video to confirm.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,62 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+
+//@ncolor <- 0
+(WHITE)
+@0
+D=A
+@ncolor
+M=D
+
+//read in keyboard:
+//If it's anything but zero, the next color is black
+(READ)
+@KBD
+D=M
+@RESETIFCHANGE
+D;JEQ
+
+//@ncolor <- -1
+//Note: we know ncolor is 0 right now. so we can just decr
+(BLACK)
+@ncolor
+M=M-1
+
+//reset @screenptr if color != ncolor 
+//if: color+ncolor+1==0, then we changed
+(RESETIFCHANGE)
+@color
+D=M
+@ncolor
+D=D+M
+D=D+1
+@SETCOLOR
+D;JNE
+@SCREEN
+D=A
+@screenptr
+M=D
+
+//@color <- @ncolor
+(SETCOLOR)
+@ncolor
+D=M
+@color
+M=D
+
+// [@screenptr] <- @color
+@color
+D=M
+@screenptr
+A=M
+M=D
+
+//@screenptr <- @screenptr +1
+@screenptr
+M=M+1
+
+@LOOP
+0;JMP
+
