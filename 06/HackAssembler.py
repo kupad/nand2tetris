e0e03001db1ac79946c9671f6ad7ffdc4394fd15
kupad <dreizen@kupad.net>
2021-12-11 00:09:26 -0500

    remove Code class
    I don't think a class is necessary to represent Code,
    unlike Parser, which I think is a natural object with state

--- 06/HackAssembler.py
+++ 06/HackAssembler.py
@@ -3,17 +3,7 @@
 
 import sys
 import re
-from collections import defaultdict
 from enum import Enum, auto
-
-# anatomy HACK C assembly instr:
-# dest=comp;JMP
-#
-# dest: D,M,A,DM,DA
-# comp: D+1, D&M
-# JGE/JMP/JEQ
-#
-# 00000000
 
 
 class InstrType(Enum):
@@ -22,12 +12,16 @@ class InstrType(Enum):
     L = auto()  # pseudo-instruction
 
 
+# represent no token
+# which is legal for dest and jmp
+EMPTYTOK = ''
+
+
 class ParserError(Exception):
     pass
 
 
 class Parser:
-    EMPTYTOK = ''
 
     def __init__(self, filename):
         self.fd = open(filename)  # the file we're assembling
@@ -45,18 +39,24 @@ class Parser:
         Takes the current C-instruction and breaks it down into
         the 3 tokens: dest, comp and jmp
         dest and jmp can be empty, only comp is mandatory
+
+        Anatomy HACK C assembly instr:
+            dest=comp;JMP
+
+            dest: D,M,A,DM,DA...
+            comp: D+1, D&M, ...
+            jmp: JGE/JMP/JEQ/...
         """
         # split instruction on '='. We have a lhs if len > 1
         eqsplit = self.instr.split('=')
-        self.desttok = eqsplit[0] if len(eqsplit) > 1 else self.EMPTYTOK
+        self.desttok = eqsplit[0] if len(eqsplit) > 1 else EMPTYTOK
 
         # split the rest on ';'
         # comp is always the leftmost.
         # we have a jmp if splitting on ';' yielded a left and right
-        rest = eqsplit[-1]
-        scsplit = rest.split(';')
+        scsplit = eqsplit[-1].split(';')
         self.comptok = scsplit[0]
-        self.jmptok = scsplit[-1] if len(scsplit) > 1 else self.EMPTYTOK
+        self.jmptok = scsplit[-1] if len(scsplit) > 1 else EMPTYTOK
 
     def advance(self):
         """advance to the next instruction"""
@@ -121,74 +121,61 @@ class CodeError(Exception):
     pass
 
 
-class Code:
+destmap = {
+    EMPTYTOK: '000',
+    'M': '001',
+    'D': '010',
+    'DM': '011',
+    'A': '100',
+    'AM': '101',
+    'AD': '110',
+    'ADM': '111',
+}
+
+compmap = {
+    "0":   "0101010",
+    "1":   "0111111",
+    "-1":  "0111010",
+    "D":   "0001100",
+    "A":   "0110000", "M": "1110000",
+    "!D":  "0001101",
+    "!A":  "0110001", "!M": "1110001",
+    "-D":  "0001111",
+    "-A":  "0110011", "-M": "1110011",
+    "D+1": "0011111",
+    "A+1": "0110111", "M+1": "1110111",
+    "D-1": "0001110",
+    "A-1": "0110010", "M-1": "1110010",
+    "D+A": "0000010", "D+M": "1000010",
+    "D-A": "0010011", "D-M": "1010011",
+    "A-D": "0000111", "M-D": "1000111",
+    "D&A": "0000000", "D&M": "1000000",
+    "D|A": "0010101", "D|M": "1010101",
+}
+
+jmpmap = {
+    EMPTYTOK: '000',
+    'JGT': '001',
+    'JEQ': '010',
+    'JGE': '011',
+    'JLT': '100',
+    'JNE': '101',
+    'JLE': '110',
+    'JMP': '111',
+}
+
+
+def cinstr(comptok, desttok, jmptok):
+    """111|comp|dest|jmp"""
+    return '111' + compmap[comptok] + destmap[desttok] + jmpmap[jmptok]
+
+
+def ainstr(symb):
     """
-    tranlates assembly tokens into binary
+    given @xxx, op is xxx
+    0{15}
     """
-    destmap = {
-        Parser.EMPTYTOK: '000',
-        'M': '001',
-        'D': '010',
-        'DM': '011',
-        'A': '100',
-        'AM': '101',
-        'AD': '110',
-        'ADM': '111',
-    }
-
-    compmap = {
-        "0":   "0101010",
-        "1":   "0111111",
-        "-1":  "0111010",
-        "D":   "0001100",
-        "A":   "0110000", "M": "1110000",
-        "!D":  "0001101",
-        "!A":  "0110001", "!M": "1110001",
-        "-D":  "0001111",
-        "-A":  "0110011", "-M": "1110011",
-        "D+1": "0011111",
-        "A+1": "0110111", "M+1": "1110111",
-        "D-1": "0001110",
-        "A-1": "0110010", "M-1": "1110010",
-        "D+A": "0000010", "D+M": "1000010",
-        "D-A": "0010011", "D-M": "1010011",
-        "A-D": "0000111", "M-D": "1000111",
-        "D&A": "0000000", "D&M": "1000000",
-        "D|A": "0010101", "D|M": "1010101",
-    }
-
-    jmpmap = {
-        Parser.EMPTYTOK: '000',
-        'JGT': '001',
-        'JEQ': '010',
-        'JGE': '011',
-        'JLT': '100',
-        'JNE': '101',
-        'JLE': '110',
-        'JMP': '111',
-    }
-
-    def dest(self, tok):
-        return self.destmap[tok]
-
-    def comp(self, tok):
-        return self.compmap[tok]
-
-    def jump(self, tok):
-        return self.jmpmap[tok]
-
-    def cinstr(self, symb):
-        """111|comp|dest|jmp"""
-        return '111' + (codegen.dest(parser.comptok) +
-                        codegen.comp(parser.desttok) +
-                        codegen.jump(parser.jmptok))
-
-    def ainstr(self, symb):
-        """
-        given @xxx, op is xxx
-        0{15}
-        """
-        return '0' + format(int(symb), '015b')
+    return '0' + format(int(symb), '015b')
 
 
 if __name__ == '__main__':
@@ -196,22 +183,18 @@ if __name__ == '__main__':
     outfilename = sys.argv[2]
 
     with open(outfilename, 'w') as outfile:
-        parser = Parser(infilename)
-        codegen = Code()
+        p = Parser(infilename)
 
-        while parser.has_next():
-            parser.advance()
-            itype = parser.instr_type()
-            """111|comp|dest|jmp"""
+        while p.has_next():
+            p.advance()
+            itype = p.instr_type()
             if itype is InstrType.C:
-                binout = '111' + (codegen.comp(parser.comptok) +
-                                  codegen.dest(parser.desttok) +
-                                  codegen.jump(parser.jmptok))
+                binout = cinstr(p.comptok, p.desttok, p.jmptok)
             elif itype is InstrType.A:
-                binout = codegen.ainstr(parser.symbol())
+                binout = ainstr(p.symbol())
             else:
                 pass
             # print(parser.instr, '->', binout)
             print(binout, file=outfile)
 
-    parser.close()
+    p.close()
